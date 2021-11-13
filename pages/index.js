25ef87412aa83ab06abaafd71f7ca4b82e146256
Solring Lin <sherry12714@gmail.com>
2021-11-13 01:00:10 +0000

    init: integrate tailwind.

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,43 @@
 import Head from 'next/head'
-import styles from './index.module.css'
+
+const box = "p-4 rounded-sm text-gray-700 border-4 border-blue-300"
 
 const Home = () => (
-  <div className={styles.container}>
+  <div className="mx-auto container">
     <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
-      <h1 className={styles.title}>
+      <h1 className="text-gray-700 text-4xl">
         Welcome to <a href="https://nextjs.org">Next.js!</a>
       </h1>
 
-      <p className={styles.description}>
+      <p className="text-green-400">
         Get started by editing <code>pages/index.js</code>
       </p>
 
-      <div className={styles.grid}>
-        <a href="https://nextjs.org/docs" className={styles.card}>
+      <div className="grid grid-cols-2 gap-3">
+        <a href="https://nextjs.org/docs" className={box}>
           <h3>Documentation &rarr;</h3>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
 
-        <a href="https://nextjs.org/learn" className={styles.card}>
+        <a href="https://nextjs.org/learn" className={box}>
           <h3>Learn &rarr;</h3>
           <p>Learn about Next.js in an interactive course with quizzes!</p>
         </a>
 
         <a
           href="https://github.com/vercel/next.js/tree/master/examples"
-          className={styles.card}
+          className={box}
         >
           <h3>Examples &rarr;</h3>
           <p>Discover and deploy boilerplate example Next.js projects.</p>
         </a>
 
-        <a href="https://vercel.com/new" className={styles.card}>
+        <a href="https://vercel.com/new" className={box}>
           <h3>Deploy &rarr;</h3>
           <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
         </a>
